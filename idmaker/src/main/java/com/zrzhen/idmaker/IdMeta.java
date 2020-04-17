bb2a5e4dc09f53f0f6662ca82027216eca407dd3
Socrates2017 <a@zrzhen.com>
2020-04-18 00:41:05 +0800

    通用id生成器

--- idmaker/src/main/java/com/zrzhen/idmaker/IdMeta.java
+++ idmaker/src/main/java/com/zrzhen/idmaker/IdMeta.java
@@ -3,38 +3,38 @@ package com.zrzhen.idmaker;
 public class IdMeta {
 
     /** 开始时间截 (2015-01-01) */
-    private final long epoch = 1500041600000L;
+    private final long epoch = 1587141600303L;
 
     /** 数据标识id所占的位数 */
     private final long datacenterIdBits = 9L;
 
     /** 机器id所占的位数 */
-    private final long workerIdBits = 1L;
+    private final long versionBits = 1L;
 
     /** 序列在id中占的位数 */
     private final long sequenceBits = 12L;
 
     /** 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
-    private final long maxWorkerId = -1L ^ (-1L << workerIdBits);
+    private final long maxVersion = -1L ^ (-1L << versionBits);
 
     /** 支持的最大数据标识id，结果是31 */
     private final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
 
     /** 机器ID向左移12位 */
-    private final long workerIdShift = sequenceBits;
+    private final long versionShift = sequenceBits;
 
     /** 数据标识id向左移17位(12+5) */
-    private final long datacenterIdShift = sequenceBits + workerIdBits;
+    private final long datacenterIdShift = sequenceBits + versionBits;
 
     /** 时间截向左移22位(5+5+12) */
-    private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+    private final long timestampLeftShift = sequenceBits + versionBits + datacenterIdBits;
 
     /** 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095) */
     private final long sequenceMask = -1L ^ (-1L << sequenceBits);
 
 
-    public long getWorkerIdShift() {
-        return workerIdShift;
+    public long getVersionShift() {
+        return versionShift;
     }
 
     public long getDatacenterIdShift() {
@@ -49,8 +49,8 @@ public class IdMeta {
         return sequenceMask;
     }
 
-    public long getMaxWorkerId() {
-        return maxWorkerId;
+    public long getMaxVersion() {
+        return maxVersion;
     }
 
     public long getMaxDatacenterId() {
